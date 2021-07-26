328b4ba86c631b3ba09c10f632b6ddf877a77efa
Said Rehouni <said_rehouni@hotmail.com>
2021-07-26 22:25:02 +0200

    Created FeedItem

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Said Rehouni on 26/7/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}

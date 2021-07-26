67bf103ec86fbcfee1af7045ebd11b114ac631bc
Said Rehouni <said_rehouni@hotmail.com>
2021-07-26 22:27:52 +0200

    Created FeedLoader protocol and its result enum

---
+++ EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Said Rehouni on 26/7/21.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping () -> Void)
+}

61c938a3f959230b545826347b99a720a839029e
Said Rehouni <said_rehouni@hotmail.com>
2021-07-27 23:58:15 +0200

    Added RemoteFeedLoader test when does not request data from URL

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Said Rehouni on 27/7/21.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}

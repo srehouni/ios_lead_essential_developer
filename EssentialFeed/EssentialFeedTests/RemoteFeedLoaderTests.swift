7393e4f0352fc0ee475c8d78689be1f71f816965
Said Rehouni <said_rehouni@hotmail.com>
2021-08-19 20:22:23 +0200

    Used explicit json data for non 200 http response test

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -50,7 +50,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         samples.enumerated().forEach { [weak client] index, code in
             expect(sut, toCompleteWithResult: .failure(.invalidData)) {
-                client?.complete(withStatusCode: code, at: index)
+                let json = makeItemJSON(items: [])
+                client?.complete(withStatusCode: code, data: json, at: index)
             }
         }
     }
@@ -142,7 +143,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(url: messages[index].url,
                                            statusCode: code,
                                            httpVersion: nil,

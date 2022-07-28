893ef95b6589b96b1f83db09072355338a9688b2
Said Rehouni <said.rehouni@kape.com>
2022-07-28 22:16:18 +0200

    Added End to End test target

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,20 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Said Rehouni on 28/7/22.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        
+    }
+}
